d76965a9bda56235c12fa807f9326b2b373bc48d
Felipe Borges <bumbleblo2013@gmail.com>
2017-10-14 15:40:10 -0300

    refactor

--- trabalho03.asm
+++ trabalho03.asm
@@ -1,5 +1,6 @@
 .data
 
+message:		.asciiz "O valor calculador com os primeiros 5 temos da série é:"
 msg_leitura: 	.asciiz "Informe o valor de arcoseno que deseja calcular: "
 fatorial: 	.word 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600
 binom: 		.word 1, 2, 6, 20, 70, 252, 924, 3432, 12870, 48620, 184756
@@ -7,66 +8,87 @@ binom: 		.word 1, 2, 6, 20, 70, 252, 924, 3432, 12870, 48620, 184756
 .text
 main:
 
+	# ---REGISTRADORES---
+	# $f6 e $f4 -auxiliar flutuante
+	# $f8 - número da leitura 
+	# $f12  - resposta
+	
+	# $s0 - contador do for 
+	# $s3  - exponenciacao de 4 
+	# $s4  - auxiliar inteiro
+	# $s5 e $s6 - vão apontar para a os binmomioes e fatorial na memória respectivamente
+	
+	# --- OBESERVAÇÃO ---
+	# * Como o número de termos da série é constante, preferimos deixar na memória para evitar alguns cáculos
+	#   Para escrever esses termos é simples, o binomio obedece a relação b(n,k) = b(n,k-1) + b(n-1,k-1) e assim
+	#   podemos usar um algoritmo de programação dinâmica para cria-los, como o foco do trabalho é ponto flutuante
+	#   preprocessamos esses termos e deixamos na memória.
+	
+	
 	la $s5, fatorial # load fatorial
 	la $s6, binom #load binom
 	
 	# exponenciacao com 4
 	add $s3, $zero, 4
 	
-	lwc1 $f8, 0($s5)
-	cvt.d.w $f8, $f8
-	
 	jal le_double	#Funcao de leitura de um double
-
-	#apenas com 5 termos da série 
 	
-	add $s0, $zero, 0 # contador do for 
+	xor $s0, $s0, $s0 #zerando contador inteiro
+	
+ 	mtc1 $zero ,$f12 # zerando $f12 que será nosso resultado 
+ 	cvt.d.w $f12, $f12 # convertendo (não é necessário mas esse passo está aqui somente pra garantir)
 	
 	for: 			
-		lwc1 $f8, ($s5) #valor de (2n)!
-		cvt.d.w $f8, $f8 #converte pra double
+	
+		#pega a exponenciacao de 4^n e converte para double  
+		mtc1 $s3, $f6
+		cvt.d.w $f6, $f6 
 		
-		#pega o 4 e converte para double 
-		mtc1 $s3, $f10 
-		cvt.d.w $f10, $f10
-		
-		#divide pela exponenciacao de 4^n
-		div.d $f8, $f8, $f10 
-		
-		sll $s3, $s3, 2 #multiplica por 4
-		
-		
-		mtc1 $zero,$f10  #seta 0 no $f10
-		cvt.d.w $f10, $f10 #converte 
+		sll $s3, $s3, 2 # já realiza a exponenciação para próxima interação  	 
 		
 		#(2n+1)
-		
-		sll $s4, $s3, 1
+		sll $s4, $s0, 1 # 2n
 		addi $s4, $s4, 1 #(2n+1)
+
+		mtc1 $s4, $f4 
+		cvt.d.w $f4, $f4
 		
-		mtc1 $s4, $f10 
-		cvt.d.w $f10, $f10
+		mul.d $f6, $f6, $f4 # termo (2n+1)*4^n 
+		div.d $f6, $f8, $f6 # realiza a divisão
 		
-		div.d $f8, $f8, $f10 #divide 
+		lwc1 $f4, 0($s6) # carrega o binomio
+		cvt.d.w $f4, $f4
 		
-  		la $s5, 4($s5)# proximo fatorial  
+		mul.d $f6, $f6, $f4 #multiplica novamente
+		
+		add.d $f12, $f12, $f6
+		
   		la $s6, 4($s6)# proximo binom
-  		
   		
   		add $s0, $s0, 1 
   		
 	bne $s0, 4, for
+	
+	la $a0, message
+	li $v0, 4
+	syscall 
+
+	li $v0, 3
+	syscall 
 
 	li $v0, 10	#Carrega o valor de encerramento do programa
 	syscall		#Encerra o programa
 
 le_double:
+
 	la $a0, msg_leitura	#Carrega endereco da string
 	li $v0, 4	#Carrega o valor referente ao print da string
 	syscall		#Chamada de sistema
 	
 	li $v0, 7	#Carrega o valor referente a leitura de um double
 	syscall		#Chamada de sistema
+
+	mov.d $f8, $f0 # salvando no lugar certo 
 	
 	jr $ra		#Retorno pra o local onde foi chamado
 
